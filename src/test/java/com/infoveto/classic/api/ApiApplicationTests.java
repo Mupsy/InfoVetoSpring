0fbfefe43a9d09b534a63d5ba9e9e04c3b83b137
Mupsy <benjibou02@gmail.com>
2025-10-16 18:51:20 +0200

    Init spring project

---
+++ src/test/java/com/infoveto/classic/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoveto.classic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
